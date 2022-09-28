f0af708e1bce828f0415e65c1caaebc2f7f3919d
manwithcode09 <113692351+manwithcode09@users.noreply.github.com>
2022-09-29 06:13:13 +0700

    Add files via upload

---
+++ Random Quiz App/js/question.js
@@ -0,0 +1,47 @@
+
+// Array dari object
+const quiz = [
+
+{
+    q : 'Lengkapilah Pribahasa berikut! <br><b>"Ada....dibalik batu."</b></br>',
+    options : ['Kontrakan Murah', 'Udang', 'kuda betina', 'Agus'],
+    answer : 1
+
+},
+
+
+{
+
+    q : 'Bila mendengar kata "<b>Terserah</b>" dari pasanganmu, sebaiknya kamu ?',
+    options : ['Pura-pura mati', 'Berubah jadi keset', 'Kabur ke Zimbabwe', 'Terima saja sambil ngelatih mental'],
+    answer : 3
+
+},
+
+
+
+{
+    q : 'Lelaki selau ?',
+    options : ['Benar', 'Salah'],
+    answer : 1
+
+},
+
+
+{
+    q : 'Saat kamu ingin mandi, lalu tiba-tiba temanmu mengirimkan kamu pesan dan menanyakan tentang posisimu, hendaknya kamu menjawab ?',
+    options : ['Sebentar, Aku mandi dulu.','Aman, ini lagi OTW.'],
+    answer : 1
+
+},
+
+
+
+{
+    q : 'Agus pergi ke Minimarket membawa uang Rp10.000.<br>Dan total belanja Agus adalah Rp9.000. Berapakah kembalian Uang Agus ?</br>',
+    options : ['Rp.1000', 'Rp.2000', 'Rp.3000', '2 Biji Permen Mentos'],
+    answer : 3
+
+}
+
+];
