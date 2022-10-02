fbf3362a971780a985e6c9c8d95738aa0a2bde3e
Caio Pessoa <caio.almeida.pessoa07@aluno.ifce.edu.br>
2022-10-01 22:15:10 -0300

    Brazilian Quizz Added

---
+++ Portuguese Quizz App/js/question.js
@@ -0,0 +1,47 @@
+
+// Array dari object
+const quiz = [
+
+{
+    q : 'Em que ano foi a Independência do Brasil?"</b></br>',
+    options : ['1822', '1823', '1500', '2022'],
+    answer : 0
+
+},
+
+
+{
+
+    q : 'Quem país colonizou o Brasil?',
+    options : ['Espanha', 'Holanda', 'Itália', 'Portugal'],
+    answer : 3
+
+},
+
+
+
+{
+    q : 'Qual a capital do Brasil?',
+    options : ['São Paulo', 'Brasília', 'Rio de Janeiro'],
+    answer : 1
+
+},
+
+
+{
+    q : 'A partir de quantos anos se pode votar no Brasil?',
+    options : ['15.','16', '17', '18'],
+    answer : 1
+
+},
+
+
+
+{
+    q : 'Quantos estados tem o Brasil?</br>',
+    options : ['12', '27', '20', '26'],
+    answer : 3
+
+}
+
+];
